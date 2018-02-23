03d6c905c64d2000406a466e830c9a0fa8ff2570
pratyush1687 <pratyush1687@gmail.com>
2018-02-23 19:52:57 +0530

    12345678

--- public/page4.js
+++ public/page4.js
@@ -8,9 +8,8 @@
         contentType: 'multipart/form-data',
         success:function(result){
           console.log("hogya submit what you wanna accomplish")
-          $.get({url:`/shuruKro`,success:function(res){
-              window.location=`/shuruKro`;
-          }});
+          console.log('result');
+          
         }
       });
 
--- server.js
+++ server.js
@@ -1,104 +1,102 @@
-const express=require('express');
-const bodyParser= require ('body-parser');
-const hbs=require('hbs');
-const app=express();
-const multer=require('multer');
+const express = require('express');
+const bodyParser = require('body-parser');
+const hbs = require('hbs');
+const app = express();
+const multer = require('multer');
 const predict = require('./predict_photo.js')
 
-const database=require('./database.js')
+const database = require('./database.js')
 
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/',express.static('public'));
+app.use('/', express.static('public'));
 
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
   //set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err:{};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   //render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-app.set('view engine','hbs' );
-app.set('views','views');
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 const multerConf = {
   storage: multer.diskStorage({
-    destination : function(req, file, next){
+    destination: function (req, file, next) {
       next(null, './public/uploads');
     },
-    filename: function(req, file, next){
+    filename: function (req, file, next) {
       const ext = file.mimetype.split('/')[1];
-      const fileName=file.fieldname + '-' +Date.now()+ '.' +ext;
+      const fileName = file.fieldname + '-' + Date.now() + '.' + ext;
 
-      next(null,fileName);
+      next(null, fileName);
       //console.log(file);
 
     }
   })
 };
 
-app.get('/shuruKro', function(req,res,next){
+app.get('/shuruKro', function (req, res) {
   console.log('in get');
-  database.getUser(function(us){
+  database.getUser(function (us) {
     console.log(us);
-    res.render('page2',{data:us});
+    res.render('page2', { data: us });
   });
 
 })
 
-app.get('/meals', function(req,res,next){
+app.get('/meals', function (req, res) {
   res.render('page3');
 })
 
 
-app.post('/shuruKro',function(req,res){
+app.post('/shuruKro', function (req, res) {
   console.log('in post');
   // console.log(req);
   // console.log(req.body)
-  var formData=req.body;
+  var formData = req.body;
   // console.log(formData);
-  var totalCal=0;
-  var Cal={};
-  if(formData.gen=="male"){
-    totalCal=66.5+ (13.8*formData.wei)+(5*formData.hei)-(6.75*formData.ag);
+  var totalCal = 0;
+  var Cal = {};
+  if (formData.gen == "male") {
+    totalCal = 66.5 + (13.8 * formData.wei) + (5 * formData.hei) - (6.75 * formData.ag);
   }
-  else{
-  totalCal=665.1+ (9.6*formData.wei)+(1.9*formData.hei)-(4.7*formData.ag);
+  else {
+    totalCal = 665.1 + (9.6 * formData.wei) + (1.9 * formData.hei) - (4.7 * formData.ag);
   }
 
 
-  Cal.totalCal=totalCal;
+  Cal.totalCal = totalCal;
   // console.log(Cal);
-  database.addTotalCalToUser(totalCal,function(err){
-    if(err) throw err;
+  database.addTotalCalToUser(totalCal, function (err) {
+    if (err) throw err;
     res.sendStatus(200);
   });
 
 });
 
-app.get('/add',function(req,res){
-    res.render('page4');           ///iss route pe uplaod waala html hogaaa
+app.get('/add', function (req, res) {
+  res.render('page4');           ///iss route pe uplaod waala html hogaaa
 })
 
-app.post('/upload', multer(multerConf).single('myImage'), function(req,res){
-
-     console.log(req.file);
-     var imgurl="https://www.foodfitnessbeautyandmore.com/wp-content/uploads/2017/03/brown-rice-dosa-chatni-sambar-aloo.jpg"
-     predict.predict(imgurl);
-
-
-  }
+app.post('/upload', multer(multerConf).single('myImage'), function (req, res) {
+  console.log(req.file);
+  var imgurl = "https://www.foodfitnessbeautyandmore.com/wp-content/uploads/2017/03/brown-rice-dosa-chatni-sambar-aloo.jpg"
+  predict.predict(imgurl, function (data) {
+    console.log(data);
+    res.redirect('/shuruKro');
+  });
+}
 );
 
 
-
-
-app.listen(7171,function(){
+app.listen(7171, function () {
   console.log('server running on port 7171');
-   database.connect();
+  database.connect();
 })
